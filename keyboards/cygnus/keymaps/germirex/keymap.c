7f3a8079a399016b28bfd792cea9341a239176dc
Helge Derenthal <helge.derenthal@posteo.de>
2025-06-25 23:19:27 +0200

    mapgerman.h and home row mods added

--- keyboards/cygnus/keymaps/germirex/keymap.c
+++ keyboards/cygnus/keymaps/germirex/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "./mapgerman.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
@@ -16,9 +17,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┘   └───┘
       */
     [0] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
-        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                               KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-        KC_Y,    KC_X,    KC_C,    KC_D,    KC_V,                               KC_K,    KC_H,    KC_COMM, KC_DOT,  S(KC_7),
-                                   KC_ESC,  KC_SPC, KC_TAB,            KC_ENT,  KC_BSPC, KC_DEL
+           KC_Q,        KC_W,        KC_F,        KC_P,    KC_B,                       KC_J,       KC_L,        KC_U,        DE_Y,     KC_QUOT,
+    LGUI_T(KC_A),LALT_T(KC_R),LCTL_T(KC_S),LSFT_T(KC_T),   KC_G,                       KC_M,RSFT_T(KC_N),RCTL_T(KC_E),LALT_T(KC_I),RGUI_T(KC_O),
+           DE_Z,        KC_X,        KC_C,        KC_D,    KC_V,                       KC_K,       KC_H,     KC_COMM,      KC_DOT,     DE_FSLH,
+                                                KC_ESC,  KC_SPC, KC_TAB,   KC_ENT,  KC_BSPC,  KC_DEL
     )
 };
---
+++ keyboards/cygnus/keymaps/germirex/mapgerman.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#define DE_Y    KC_Z        // Y
+#define DE_Z    KC_Y        // Z
+#define DE_PLUS RBKT        // +
+#define DE_MINS FSLH        // -
+#define DE_USCR LS(FSLH)    // _
+
+#define DE_FSLH S(KC_7)     // /
+#define DE_STAR LS(RBKT)    // *
+
+#define DE_SEMI LS(COMMA)   // ;
+#define DE_COLO LS(DOT)     // :
+
+#define DE_AT   RA(Q)       // @
+#define DE_ae   APOS        // ä
+#define DE_Ae   LS(APOS)    // Ä
+#define DE_SS   MINUS       // ß
+#define DE_LBKT RA(N8)      // [
+#define DE_RBKT RA(N9)      // ]
+#define DE_LBRC RA(N7)      // {
+#define DE_RBRC RA(N0)      // }
+#define DE_LT   NUBS        // <
+#define DE_GT   LS(NUBS)    // >
+#define DE_LPAR LS(N8)      // (
+#define DE_RPAR LS(N9)      // )
+#define DE_EURO RA(E)       // €
+#define DE_DLLR LS(N4)      // $
+#define DE_AMPS LS(N6)      // &
+#define DE_PRCT LS(N5)      // %
+#define DE_DEGR LS(GRAVE)   // °
+
+#define DE_HASH NUHS        // #
+#define DE_TILD RA(RBKT)    // ~
+#define DE_EQL  LS(N0)      // =
+#define DE_GRV  LS(EQUAL)   // `
+#define DE_ACUT EQUAL       // ´
+#define DE_DQT  LS(N2)      // "
+#define DE_SQT  LS(NUHS)    // '
+#define DE_ue   LBKT        // ü
+#define DE_Ue   LS(LBKT)    // Ü
+#define DE_EXLM LS(N1)      // !
+#define DE_QUES LS(MINUS)   // ?
+#define DE_BSLH RA(MINUS)   // Backslash
+#define DE_PIPE RA(NUBS)    // |
+#define DE_CIRC GRAVE       // ^
+#define DE_oe   SEMI        // ö
+#define DE_Oe   LS(SEMI)    // Ö
+#define DE_SECT LS(N3)      // §
