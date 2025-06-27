856a0aaf00b3db7f294becd9b9f4cbf2378242bd
Helge Derenthal <helge.derenthal@posteo.de>
2025-06-27 21:00:37 +0200

    Mouse and Symbol Layer added

--- keyboards/cygnus/keymaps/germirex/mapgerman.h
+++ keyboards/cygnus/keymaps/germirex/mapgerman.h
@@ -1,49 +1,49 @@
 #pragma once
 
-#define DE_Y    KC_Z        // Y
-#define DE_Z    KC_Y        // Z
-#define DE_PLUS RBKT        // +
-#define DE_MINS FSLH        // -
-#define DE_USCR LS(FSLH)    // _
+#define DE_Y    KC_Z         // Y
+#define DE_Z    KC_Y         // Z
+#define DE_PLUS KC_RBRC      // +
+#define DE_MINS KC_SLSH      // -
+#define DE_USCR S(KC_SLSH)   // _
 
-#define DE_FSLH S(KC_7)     // /
-#define DE_STAR LS(RBKT)    // *
+#define DE_FSLH S(KC_7)      // /
+#define DE_STAR S(KC_RBRC)   // *
 
-#define DE_SEMI LS(COMMA)   // ;
-#define DE_COLO LS(DOT)     // :
+#define DE_SEMI S(KC_COM)    // ;
+#define DE_COLO S(KC_DOT)    // :
 
-#define DE_AT   RA(Q)       // @
-#define DE_ae   APOS        // ä
-#define DE_Ae   LS(APOS)    // Ä
-#define DE_SS   MINUS       // ß
-#define DE_LBKT RA(N8)      // [
-#define DE_RBKT RA(N9)      // ]
-#define DE_LBRC RA(N7)      // {
-#define DE_RBRC RA(N0)      // }
-#define DE_LT   NUBS        // <
-#define DE_GT   LS(NUBS)    // >
-#define DE_LPAR LS(N8)      // (
-#define DE_RPAR LS(N9)      // )
-#define DE_EURO RA(E)       // €
-#define DE_DLLR LS(N4)      // $
-#define DE_AMPS LS(N6)      // &
-#define DE_PRCT LS(N5)      // %
-#define DE_DEGR LS(GRAVE)   // °
+#define DE_AT   RALT(KC_Q)   // @
+#define DE_ae   KC_QUOT      // ä
+#define DE_Ae   S(KC_QUOT)   // Ä
+#define DE_SS   KC_MINS      // ß
+#define DE_LBKT RALT(KC_8)   // [
+#define DE_RBKT RALT(KC_9)   // ]
+#define DE_LBRC RALT(KC_7)   // {
+#define DE_RBRC RALT(KC_0)   // }
+#define DE_LT   KC_NUBS      // <
+#define DE_GT   S(KC_NUBS)   // >
+#define DE_LPAR S(KC_8)      // (
+#define DE_RPAR S(KC_9)      // )
+#define DE_EURO RALT(KC_E)   // €
+#define DE_DLLR S(KC_4)      // $
+#define DE_AMPS S(KC_6)      // &
+#define DE_PRCT S(KC_5)      // %
+#define DE_DEGR S(KC_GRV)    // °
 
-#define DE_HASH NUHS        // #
-#define DE_TILD RA(RBKT)    // ~
-#define DE_EQL  LS(N0)      // =
-#define DE_GRV  LS(EQUAL)   // `
-#define DE_ACUT EQUAL       // ´
-#define DE_DQT  LS(N2)      // "
-#define DE_SQT  LS(NUHS)    // '
-#define DE_ue   LBKT        // ü
-#define DE_Ue   LS(LBKT)    // Ü
-#define DE_EXLM LS(N1)      // !
-#define DE_QUES LS(MINUS)   // ?
-#define DE_BSLH RA(MINUS)   // Backslash
-#define DE_PIPE RA(NUBS)    // |
-#define DE_CIRC GRAVE       // ^
-#define DE_oe   SEMI        // ö
-#define DE_Oe   LS(SEMI)    // Ö
-#define DE_SECT LS(N3)      // §
+#define DE_HASH KC_NUHS      // #
+#define DE_TILD RALT(KC_RBRC)// ~
+#define DE_EQL  S(KC_0)      // =
+#define DE_GRV  S(KC_EQL)    // `
+#define DE_ACUT KC_EQL       // ´
+#define DE_DQT  S(KC_2)      // "
+#define DE_SQT  S(KC_NUHS)   // '
+#define DE_ue   KC_LBRC      // ü
+#define DE_Ue   S(KC_LBRC)   // Ü
+#define DE_EXLM S(KC_1)      // !
+#define DE_QUES S(KC_MINS)   // ?
+#define DE_BSLH RALT(KC_MINS)// Backslash
+#define DE_PIPE RALT(KC_NUBS)// |
+#define DE_CIRC KC_GRV       // ^
+#define DE_oe   KC_SCLN      // ö
+#define DE_Oe   S(KC_SCLN)   // Ö
+#define DE_SECT S(KC_3)      // §
